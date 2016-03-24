de78f71d427e1cc3377bc270b6e5273d7817b000
daniellef <daniellefarias813@gmail.com>
2016-03-23 21:04:55 -0700

    9 initializing and adding

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,2 +1,13 @@
-
-
+$(function (){
+    var APPLICATION_ID = "0A62B1D0-15B8-F2F1-FF35-45FA80A78900",
+        SECRET_KEY = "196BD88C-948D-1640-FFD5-1B9DFBED0000",
+        VERSION = "v1";
+        
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+        
+    var user = new Backendless.user();
+    user.email = "daniellefarias813@gmail.com";
+    user.password = "Cinnamon";
+    Backendless.userService.request(user);
+    
+});
