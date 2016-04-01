547046a5dc897c2f12da3b0751135fce2c8b05fb
daniellef <daniellefarias813@gmail.com>
2016-04-01 09:18:43 -0700

    23 i logged in :]

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -30,8 +30,14 @@ $(function (){
         
     }
 
-function userLoggedIn() {
+function userLoggedIn(user) {
     console.log("user successfully logged in");
+    
+    var welcomeScript = $('#welcome-template').html();
+    var welcomeTemplate = Handlebars.compile(welcomeScript);
+    var welcomeHTML = welcomeTemplate(user);
+    
+    $('.main-container').html(welcomeHTML);
 }
 
 function gotError(error) {
